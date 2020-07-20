a95f301cf213561a8b9d8832a6e8dd63ff531b28
jennifer-yoo <jennifer.yoo7@gmail.com>
2020-07-20 10:04:39 -0400

    one to many

--- author.rb
+++ author.rb
@@ -1,1 +1,37 @@
+class Author
+    attr_accessor :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def write_books(title, word_count)
+        Book.new(self, title, word_count)
+    end
+
+    def total_words
+        Book.all.select { |b_instance| b_instance.author == self
+        }.sum { |instance| instance.word_count }
+    end
+
+    def self.most_words
+        words = 0
+        wordy_author = nil
+
+        self.all.each do |instance|
+            if instance.total_words > words
+                words = instance.total_words
+                wordy_author = instance
+            end
+        end
+        wordy_author
+    end
+
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,16 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+jenn = Author.new("Jenn")
+sam = Author.new("Sam")
+bill = Author.new("Bill")
+jim = Author.new("Jim")
+
+b1 = Book.new(jenn, "title1", 1000)
+b2 = Book.new(sam, "title2", 400)
+b3 = Book.new(bill, "title3", 20000)
+b4 = Book.new(sam, "title4", 200)
+
 binding.pry
 
-
+"something"
